4dd4274471d7675b8983b17a7962ddd2c30521a4
Fumbles <campbeh3@wwu.edu>
2021-06-24 13:21:19 -0700

    messed with mastertest

--- Modules/master test.py
+++ Modules/master test.py
@@ -4,8 +4,16 @@ Created on Mon Jun 21 15:35:14 2021
 
 @author: Table
 """
-
+import pathlib
 
 from master import *
 
-Brackett_Decrement_Plot(8886,57411,3)
+#Brackett_Decrement_Plot(6224,56561,183)
+
+print(Localpath())
+
+# if str(pathlib.Path(__file__).parent.absolute()) == 'C:\\Users\\Table\\Documents\\GitHub\\T-Tauri\\Modules':
+#     print('true')
+    
+# else:
+#     print('false')
